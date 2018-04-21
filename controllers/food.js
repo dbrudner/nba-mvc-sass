b6c8897d41ca36cfe44818bcf9d8ecc0435291e1
dbrudner <dbrudne1@gmail.com>
2018-04-21 14:11:42 -0400

    templating

--- controllers/food.js
+++ controllers/food.js
@@ -1,11 +1,11 @@
 const {key} = require('../key')
 const axios = require('axios')
 
-const foodSearch = (term, res) => {
+const foodSearch = (term, render) => {
     const searchUrl = `https://api.nal.usda.gov/ndb/search/?format=json&q=${term}&sort=n&max=500&offset=0&api_key=${key}`
 
     axios.get(searchUrl).then(result => {
-        res.json(result.data.list);
+        res.render('search', result.data.list);
     })
 }
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const food = require('./food');
 
 const router = express.Router();
 
+const {key} = require('../key')
+const axios = require('axios')
 
 router.get('/', function(req, res) {
 	res.render('index')
@@ -10,7 +11,23 @@ router.get('/', function(req, res) {
 
 router.get('/search/:term', (req, res) => {
 	const term = req.params.term
-	food.foodSearch(term, res)
+	// food.foodSearch(term, res.render)
+	const searchUrl = `https://api.nal.usda.gov/ndb/search/?format=json&q=${term}&sort=n&max=500&offset=0&api_key=${key}`
+
+    axios.get(searchUrl).then(result => {
+		console.log(result.data.list);
+		const foods = result.data.list.item
+
+		const totalResults = (foods.length === 500 ? "500+" : foods.length);
+
+
+		const hbsObject = {
+			foods, 
+			totalResults
+		}
+
+        res.render('search-results', hbsObject);
+    })
 })
 
 module.exports = router;
