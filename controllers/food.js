84e7b0d30e161a58f44582102f71abcc2269c0ce
dbrudner <dbrudne1@gmail.com>
2018-04-21 13:16:11 -0400

    commit

--- controllers/food.js
+++ controllers/food.js
@@ -1,12 +1,14 @@
 const {key} = require('../key')
 const axios = require('axios')
 
-const foodSearch = term => {
-    const searchUrl = `https://api.nal.usda.gov/ndb/search/?format=json&q=${term}&sort=n&max=25&offset=0&api_key=${key}`
+const foodSearch = (term, res) => {
+    const searchUrl = `https://api.nal.usda.gov/ndb/search/?format=json&q=${term}&sort=n&max=500&offset=0&api_key=${key}`
 
-    axios.get(searchUrl).then(res => {
-        console.log(res);
+    axios.get(searchUrl).then(result => {
+        res.json(result.data.list);
     })
 }
 
-foodSearch('butter');
+module.exports = {
+    foodSearch
+}
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const food = require('./food');
 
 const router = express.Router();
 
 
 router.get('/', function(req, res) {
 	res.render('index')
+})
+
+router.get('/search/:term', (req, res) => {
+	const term = req.params.term
+	food.foodSearch(term, res)
 })
 
 module.exports = router;
