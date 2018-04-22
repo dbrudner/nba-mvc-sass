247877d4621687a5a298c8251379b568e4fbdc43
dbrudner <dbrudne1@gmail.com>
2018-04-21 21:59:39 -0400

    fixing bugs, header finished, other stuff

--- controllers/index.js
+++ controllers/index.js
@@ -12,18 +12,16 @@ router.get('/', function(req, res) {
 
 router.get('/search/:term', (req, res) => {
 	const term = req.params.term
-	// food.foodSearch(term, res.render)
 	const searchUrl = `https://api.nal.usda.gov/ndb/search/?format=json&q=${term}&sort=n&max=500&offset=0&api_key=${key}`
 
     axios.get(searchUrl).then(result => {
-		console.log(result.data.list);
-		const foods = result.data.list.item
+		if (result.data.errors) {
+			return res.render('search-results', {error: "No results"});
+		}
 
+		const foods = result.data.list.item
 		const totalResults = (foods.length === 500 ? "500+" : foods.length);
 		const tooLarge = (foods.length === 500 ? true : false)
-
-		console.log(foods);
-
 		const hbsObject = {
 			foods, 
 			totalResults,
@@ -36,19 +34,24 @@ router.get('/search/:term', (req, res) => {
 
 router.get('/food/:ndbno', (req, res) => {
 
+	// This is the id the API uses to find a specific food
 	const {ndbno} = req.params
 
 	const searchUrl = `https://api.nal.usda.gov/ndb/V2/reports?ndbno=${ndbno}&type=b&format=json&api_key=${key}`
 
 	axios.get(searchUrl).then(result => {
 		let food = result.data.foods[0].food
-
-		// Capitalize each word in ingredient string
-		let capitalizedIngredients = food.ing.desc.split(' ').map(ing => _.capitalize(ing)).join(' ')
-
-		food = {...food, capitalizedIngredients}
-
 		console.log(food);
+		// Capitalize each word in ingredient string
+
+		if (food.ing) {
+			let capitalizedIngredients = food.ing.desc.split(' ').map(ing => _.capitalize(ing)).join(' ')
+
+			food = {...food, capitalizedIngredients}
+		}
+		
+
+
 
 		res.json(food);
 	})
